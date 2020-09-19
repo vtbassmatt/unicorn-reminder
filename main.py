39c40c2e0c6677f17b15499ca69f6654615230b7
Matt Cooper <mattc@xbox.com>
2020-09-19 06:23:12 -0400

    aesthetics

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import logging
-import sys
 import random
+import sys
+import time
+
+log_format = '[%(asctime)s %(levelname)s] %(message)s'
+date_format = '%Y-%m-%d %H:%M:%S'
+logging.basicConfig(format=log_format, datefmt=date_format, level=logging.DEBUG)
 
 from unicorn import unicorn
 
 def main():
-    from time import sleep
     unicorn.clear()
     while True:
         unicorn.set_pixel(random.randrange(0, 16), random.randrange(0, 16), random.randrange(16, 64) * 4 - 1, 0, 0)
         unicorn.show()
-        sleep(.25)
+        time.sleep(.25)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-
     try:
         main()
     except KeyboardInterrupt:
         unicorn.off()
-        logging.debug('\nand we\'re out\n')
+        logging.debug('and we\'re out')
         sys.exit(0)
