4d6afbd52f74b7099f6e209af06ab04a1fc48eba
Matt Cooper <mattc@xbox.com>
2020-09-19 07:45:17 -0400

    heartbeat with FPS

--- scene.py
+++ scene.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from unicorn import unicorn
@@ -7,14 +8,27 @@ class Scene:
     def __init__(self, unicorn):
         self.unicorn = unicorn
         self.unicorn.clear()
+        self.last_pixel = 0
 
     def update(self, current, elapsed):
+        if current - self.last_pixel >= 0.1:
+            self.unicorn.set_pixel(
+                random.randrange(0, 16),
+                random.randrange(0, 16),
+                random.randrange(16, 64) * 4 - 1,
+                0,
+                0,
+            )
+
+            self.last_pixel = current
+        
+        heartbeat = math.floor((math.sin(elapsed) + 1.0) * 127)
         self.unicorn.set_pixel(
-            random.randrange(0, 16),
-            random.randrange(0, 16),
-            random.randrange(16, 64) * 4 - 1,
             0,
             0,
+            0,
+            0,
+            heartbeat,
         )
 
     def render(self):
